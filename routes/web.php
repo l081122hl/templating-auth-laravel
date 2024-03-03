11a97b8f8466cee3fdb0b3d1021375f74312b326
Lius <liusfalaga27@gmail.com>
2024-03-03 17:58:10 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,13 @@ Route::group(['middleware' => 'auth:user'], function (){
         Route::get('/',[App\Http\Controllers\dashboardController::class, 'index'])->name('dashboard.index');
         Route::get('/profile',[App\Http\Controllers\dashboardController::class, 'profile'])->name('dashboard.profile');
 
+        Route::get('/kategori', [App\Http\Controllers\kategoriController::class, 'index'])->name('kategori.index');
+        Route::get('/kategori/tambah', [App\Http\Controllers\kategoriController::class, 'tambah'])->name('kategori.tambah');
+        Route::post('/kategori/prosesTambah', [App\Http\Controllers\kategoriController::class, 'prosesTambah'])->name('kategori.prosesTambah');
+
+        Route::get('/kategori/ubah/{id}', [App\Http\Controllers\kategoriController::class, 'ubah'])->name('kategori.ubah');
+        Route::post('/kategori/prosesUbah', [App\Http\Controllers\kategoriController::class, 'prosesUbah'])->name('kategori.prosesUbah');
+        Route::get('/kategori/hapus/{id}', [App\Http\Controllers\kategoriController::class, 'hapus'])->name('kategori.hapus');
     });
 
     route::get('/logout',[App\Http\Controllers\AuthController::class, 'logout'])->name('auth.logout');
